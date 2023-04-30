9eafec98776323202f703b1966e3132dc1f80887
Maddy011 <77790575+Maddy011@users.noreply.github.com>
2023-04-30 23:14:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-
-import ai21
+import requests
 import streamlit as st
+import ai21
 from dotenv import load_dotenv
 
 # Load secrets
@@ -9,41 +9,42 @@ load_dotenv()
 
 API_KEY = os.getenv("AI21_LABS_API_KEY")
 
-ai21.api_key = API_KEY
+# Set up ai21 API key
+ai21.api_key = 'NKz8j9xx3PpYS32lr40sPSm4oQJacbx0' # API_KEY
 
-PROMPT = "Based on the description given, name the sport.\nDescription: {description}\n Sport name: "
+# Set up News API endpoint
+NEWS_API_KEY = "b95dbe8383ec43739e32c71fbc516bb6"
+url = f"https://newsapi.org/v2/everything?apiKey={NEWS_API_KEY}"
 
-# Initialization
-if "output" not in st.session_state:
-    st.session_state["output"] = "Output:"
+st.title("AI Sports News")
 
+# Get user topic input
+topic = st.text_input("Enter a topic you want to read about in sports:")
 
-def guess_sport(inp):
-    if not len(inp):
-        return None
+if st.button("Search"):
+    # Set query parameters and fetch news articles from the API
+    params = {"q": topic, "sortBy": "relevancy", "language": "en"}
+    response = requests.get(url, params=params)
+    articles = response.json()["articles"]
 
-    prompt = PROMPT.format(description=inp)
+    # Process the articles and get predicted sports from AI model
+    for article in articles:
+        title = article["title"]
+        content = article["description"]
+        prompt = None
+        response = ai21.Completion.execute(
+            model="j2-large",
+            custom_model="ASBA-j2-large-v2",
+            prompt="YOUR_PROMPT_TEXT",
+            numResults=1,
+            maxTokens=200,
+            temperature=0.7,
+            topKReturn=0
+        )
+        sport = response.choices[0].text.strip()
 
-    response = ai21.Completion.execute(
-        model="j2-grande-instruct",
-        prompt=prompt,
-        temperature=0.5,
-        minTokens=1,
-        maxTokens=15,
-        numResults=1,
-    )
-
-    st.session_state["output"] = response.completions[0].data.text
-    st.balloons()
-
-
-st.title("The Sports Guesser")
-
-st.write(
-    "This is a simple **Streamlit** app that generates Sport Name based on given description"
-)
-
-
-inp = st.text_area("Enter your description here", height=100)
-st.button("Gue" "ss", on_click=guess_sport(inp))
-st.write(f"Answer: {st.session_state.output}")
+        # Display the predicted sport for each article
+        st.write(f"Article title: {title}")
+        st.write(f"Predicted sport: {sport}")
+        st.write(f"Article content: {content}")
+        st.write("---")
