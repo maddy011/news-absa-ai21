d6cc190ed8c40785070d5e3e7edfaaa2d926853b
Maddy011 <77790575+Maddy011@users.noreply.github.com>
2023-05-08 09:00:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import datetime
 load_dotenv()
 
 # Set up ai21 API key 
-ai21.api_key = 'your_ai21_api_key_here'
+ai21.api_key = '6261742702:AAGkil1tWLckpBbC088rTO3Lb-lH2hJVmBg'
 
 # Set up News API endpoint
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
